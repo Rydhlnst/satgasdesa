8bcdd3a15722ea301655f43e417c270fdaf51638
omenaai <omenaai.hub@gmail.com>
2026-08-21 07:56:35 +0700

    Fix dashboard tooltip provider

--- components/providers.tsx
+++ components/providers.tsx
@@ -4,14 +4,17 @@ import type { ReactNode } from "react";
 import { ThemeProvider } from "next-themes";
 
 import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 type AppProvidersProps = { children: ReactNode };
 
 export function AppProviders({ children }: AppProvidersProps) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="light" enableSystem storageKey="satgas-theme">
-      {children}
-      <Toaster closeButton position="bottom-right" />
-    </ThemeProvider>
+    <TooltipProvider>
+      <ThemeProvider attribute="class" defaultTheme="light" enableSystem storageKey="satgas-theme">
+        {children}
+        <Toaster closeButton position="bottom-right" />
+      </ThemeProvider>
+    </TooltipProvider>
   );
 }
